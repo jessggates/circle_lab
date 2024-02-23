404886afe4c10df95120fb274842324be464759f
Jessica Gates <gatespj@outlook.com>
2024-02-22 22:47:20 -0500

    working lab, need to add validation for radius

---
+++ Circle Lab/CircleLab/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleLab
+
+{
+    internal class Circle
+    {
+        private double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateDiameter(double radius)
+        {
+            return radius * 2;
+        }
+
+        public double CalculateCircumference(double radius)
+        {
+            return CalculateDiameter(radius) * double.Pi;
+        }
+
+        public double CalculateArea(double radius)
+        {
+            return radius * radius * double.Pi;
+        }
+        public void Grow() 
+        {
+            Radius = Radius * 2;
+        }
+
+        public double GetRadius()
+        {
+            return Radius;
+        }
+
+    }
+}
---
+++ Circle Lab/CircleLab/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using CircleLab;
+using System.Net.NetworkInformation;
+
+
+
+Console.WriteLine("Welcome to the Circle Tester");
+Console.Write("Enter radius: ");
+double radius = double.Parse(Console.ReadLine());
+Circle circle = new Circle(radius);
+//add validation here for valid radius entered
+bool grow = true;
+
+while (grow)
+{
+    Console.WriteLine($"Diameter: {circle.CalculateDiameter(radius)}");
+    Console.WriteLine($"Circumference: {circle.CalculateCircumference(radius)}");
+    Console.WriteLine($"Area: {circle.CalculateArea(radius)}");
+
+    Console.WriteLine("Should the circle grow? (y/n)");
+    grow = Console.ReadLine().ToLower().Trim() == "y";
+
+    if (grow == true)
+    {
+        circle.Grow();
+        Console.WriteLine("The circle is magically growing!");
+        radius = circle.GetRadius();
+    }
+    else
+    {
+        grow = false;
+        Console.WriteLine($"Goodbye. The circle's final radius is {circle.GetRadius()}");
+    }
+}
+
+Console.ReadKey();
+//bool validRadius(double radius)
+//{
+//   try 
+
+//     catch?
+//}
