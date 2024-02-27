20ec2ce8f2a81da57eaafc09001a30b634139b66
Jessica Gates <gatespj@outlook.com>
2024-02-26 20:13:46 -0500

    final lab working

--- Circle Lab/CircleLab/Program.cs
+++ Circle Lab/CircleLab/Program.cs
@@ -1,42 +1,56 @@
 ﻿
 using CircleLab;
+using System.ComponentModel.Design;
 using System.Net.NetworkInformation;
-
-
+using System.Runtime.InteropServices;
 
 Console.WriteLine("Welcome to the Circle Tester");
-Console.Write("Enter radius: ");
-double radius = double.Parse(Console.ReadLine());
-Circle circle = new Circle(radius);
-//add validation here for valid radius entered
-bool grow = true;
 
-while (grow)
+double radius = 0;
+bool validRadius = false;
+
+do
 {
-    Console.WriteLine($"Diameter: {circle.CalculateDiameter(radius)}");
-    Console.WriteLine($"Circumference: {circle.CalculateCircumference(radius)}");
-    Console.WriteLine($"Area: {circle.CalculateArea(radius)}");
-
-    Console.WriteLine("Should the circle grow? (y/n)");
-    grow = Console.ReadLine().ToLower().Trim() == "y";
-
-    if (grow == true)
+    Console.Write("Enter radius: ");
+    string input = Console.ReadLine();
+    validRadius = TryParse(input, out radius);
+    if (validRadius == false)
     {
-        circle.Grow();
-        Console.WriteLine("The circle is magically growing!");
-        radius = circle.GetRadius();
+        Console.WriteLine("That is not a valid radius, please enter a number.");
     }
     else
     {
-        grow = false;
-        Console.WriteLine($"Goodbye. The circle's final radius is {circle.GetRadius()}");
+        Circle circle = new Circle(radius);
+        bool grow = true;
+
+        while (grow)
+        {
+            Console.WriteLine($"Diameter: {circle.CalculateDiameter(radius)}");
+            Console.WriteLine($"Circumference: {circle.CalculateCircumference(radius)}");
+            Console.WriteLine($"Area: {circle.CalculateArea(radius)}");
+
+            Console.WriteLine("Should the circle grow? (y/n)");
+            grow = Console.ReadLine().ToLower().Trim() == "y";
+
+            if (grow == true)
+            {
+                circle.Grow();
+                Console.WriteLine("The circle is magically growing!");
+                radius = circle.GetRadius();
+            }
+            else
+            {
+                grow = false;
+                Console.WriteLine($"Goodbye. The circle's final radius is {circle.GetRadius()}");
+            }
+        }
     }
+} while (validRadius == false);
+
+
+static bool TryParse(string stringToParse, out double result)
+{
+    return double.TryParse(stringToParse, out result);
 }
 
 Console.ReadKey();
-//bool validRadius(double radius)
-//{
-//   try 
-
-//     catch?
-//}
